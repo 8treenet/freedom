65953f4706ac13c4aefe7cf6601689bed58a2495
杨树 <4932004@qq.com>
2020-03-03 16:56:21 +0800

    fix template

--- freedom/cmd/version.go
+++ freedom/cmd/version.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	versionNum = "v1.5.2"
+	versionNum = "v1.5.3"
 )
 
 var (
--- freedom/template/project/infra.go
+++ freedom/template/project/infra.go
@@ -88,78 +88,75 @@ func jsonResponseTemplate() string {
 	result := `
 	package infra
 
-import (
-	"github.com/8treenet/extjson"
-	"github.com/8treenet/freedom/general"
-	"github.com/kataras/iris"
-	"github.com/kataras/iris/context"
-	"github.com/kataras/iris/hero"
-)
-
-type JSONResponse struct {
-	// Code automatically sets default 501 if the error is not empty
-	Code        int
-	Err         error
-	contentType string
-	content     []byte
-
-	// If not nil then it will fire that as "application/json" or the
-	// "ContentType" if not empty.
-	Object interface{}
-
-	// If Path is not empty then it will redirect
-	// the client to this Path, if Code is >= 300 and < 400
-	// then it will use that Code to do the redirection, otherwise
-	// StatusFound(302) or StatusSeeOther(303) for post methods will be used.
-	// Except when err != nil.
-	Path string
-}
-
-func (jrep JSONResponse) Dispatch(ctx context.Context) {
-	jrep.contentType = "application/json"
-
-	if jrep.Path != "" && jrep.Err == nil {
-		// it's not a redirect valid status
-		if jrep.Code < 300 || jrep.Code >= 400 {
-			if ctx.Method() == "POST" {
-				jrep.Code = 303 // StatusSeeOther
+	import (
+		"github.com/8treenet/extjson"
+		"github.com/8treenet/freedom/general"
+		"github.com/kataras/iris"
+		"github.com/kataras/iris/context"
+		"github.com/kataras/iris/hero"
+	)
+	
+	type JSONResponse struct {
+		// Code automatically sets default 501 if the error is not empty
+		Code        int
+		Error       error
+		contentType string
+		content     []byte
+	
+		// If not nil then it will fire that as "application/json" or the
+		// "ContentType" if not empty.
+		Object interface{}
+	
+		// If Path is not empty then it will redirect
+		// the client to this Path, if Code is >= 300 and < 400
+		// then it will use that Code to do the redirection, otherwise
+		// StatusFound(302) or StatusSeeOther(303) for post methods will be used.
+		// Except when err != nil.
+		Path string
+	}
+	
+	func (jrep JSONResponse) Dispatch(ctx context.Context) {
+		jrep.contentType = "application/json"
+	
+		if jrep.Path != "" && jrep.Error == nil {
+			// it's not a redirect valid status
+			if jrep.Code < 300 || jrep.Code >= 400 {
+				if ctx.Method() == "POST" {
+					jrep.Code = 303 // StatusSeeOther
+				}
+				jrep.Code = 302 // StatusFound
 			}
-			jrep.Code = 302 // StatusFound
-		}
-		ctx.Redirect(jrep.Path, jrep.Code)
-		return
-	}
-
-	var repData struct {
-		Code int
-		Err  string      %sjson:"error"%s
-		Data interface{} %sjson:"data,omitempty"%s
-	}
-	repData.Data = jrep.Object
-	repData.Code = jrep.Code
-	if jrep.Err != nil {
-		repData.Err = jrep.Err.Error()
-	}
-	if repData.Err != "" && repData.Code == 0 {
-		repData.Code = iris.StatusNotImplemented
-	}
-	loggger := ctx.Values().Get("logger_impl")
-
-	var jsonErr error
-	jrep.content, jsonErr = extjson.Marshal(repData)
-	if loggger != nil {
-		log := loggger.(general.Logger)
-		if jsonErr != nil {
-			log.Info("JSONResponse json error:", jsonErr, "from data", repData)
+			ctx.Redirect(jrep.Path, jrep.Code)
 			return
 		}
-		log.Info("JSONResponse:", string(jrep.content))
-	}
-	if jsonErr != nil {
-		return
-	}
-	hero.DispatchCommon(ctx, jrep.Code, jrep.contentType, jrep.content, nil, jrep.Err, true)
-}`
+	
+		var repData struct {
+			Code  int
+			Error string       %sjson:"error"%s
+			Data  interface{}  %sjson:"data,omitempty"%s
+		}
+		repData.Data = jrep.Object
+		repData.Code = jrep.Code
+		if jrep.Error != nil {
+			repData.Error = jrep.Error.Error()
+		}
+		if repData.Error != "" && repData.Code == 0 {
+			repData.Code = iris.StatusNotImplemented
+		}
+		loggger := ctx.Values().Get("logger_impl")
+	
+		var jsonErr error
+		jrep.content, jsonErr = extjson.Marshal(repData)
+		if loggger != nil {
+			log := loggger.(general.Logger)
+			if jsonErr != nil {
+				log.Info("JSONResponse json error:", jsonErr, "from data", repData)
+				return
+			}
+			log.Info("JSONResponse:", string(jrep.content))
+		}
+		hero.DispatchCommon(ctx, jrep.Code, jrep.contentType, jrep.content, nil, nil, true)
+	}`
 
 	return fmt.Sprintf(result, "`", "`", "`", "`")
 }
