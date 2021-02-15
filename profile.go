7a15810e9ab86d89059c634d10d0e1fd6e9aea21
miraclecoco <miraclecoco2001@gmail.com>
2021-02-15 11:38:11 +0800

    refactor(): fix typo.

--- profile.go
+++ profile.go
@@ -17,14 +17,14 @@ var (
 	configurator Configurator
 
 	// EnvProfileDir is the name of the environment variable for the search
-	// directory of the profile
+	// directory of the profile.
 	EnvProfileDir = "FREEDOM_PROJECT_CONFIG"
 
-	// ProfileENV is the name of the profile directory in environment variable
+	// ProfileENV is the name of the profile directory in environment variable.
 	ProfileENV = EnvProfileDir
 
+	// cachedProfileDir is the cache of the profile's path.
 	// TODO(coco): this variable seems has no effect, considering remove it.
-	// cachedProfileDir is the cache of the profile path
 	cachedProfileDir string
 )
 
